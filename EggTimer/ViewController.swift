5afc40e80a3ae776848afde3159b377e79c87e6f
Donna <donnajun1214@gmail.com>
2022-05-23 16:04:17 +0900

    if else statement

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func selectedHardness(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        if(hardness == "soft"){
+            print(softTime)
+        }else if(hardness == "medium"){
+            print(mediumTime)
+        }else{
+            print(hardTime)
+        }
     }
-
 
 }
 
