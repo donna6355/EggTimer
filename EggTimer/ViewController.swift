afd1d5fea056026e9f75171551196c08c18379fe
Donna <donnajun1214@gmail.com>
2022-05-24 10:07:58 +0900

    reset timer and set alarm sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,31 +6,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleUI: UILabel!
     
     let eggTimes = ["soft":5,"medium":7, "hard":12] //dictionaries
-    var counter = 0
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
     
 
     @IBAction func selectedHardness(_ sender: UIButton) {
-        timer.invalidate()
+        
         let hardness = sender.currentTitle!
-        counter = eggTimes[hardness]!
+        
+        timer.invalidate()
+        progressBar.progress = 0
+        secondsPassed = 0
+        titleUI.text = hardness
+        
+        totalTime = eggTimes[hardness]!
 
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(updateCounter), userInfo: nil, repeats: true)
     }
 
     @objc func updateCounter(){
-         if counter > 0 {
-             print("\(counter) seconds")
-             counter -= 1
+         if totalTime > secondsPassed {
+             secondsPassed += 1
+             progressBar.progress = Float(secondsPassed)/Float(totalTime)
          }else{
              timer.invalidate()
              titleUI.text = "DONE!"
+             let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+             player = try! AVAudioPlayer(contentsOf: url!)
+             player.play()
+
          }
     }
 }
